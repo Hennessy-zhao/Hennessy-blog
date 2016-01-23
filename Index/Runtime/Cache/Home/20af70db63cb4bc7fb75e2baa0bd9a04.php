78278ffe30e622f7bd0e652e76223e452cfaf566
Hennessy.zhao <834819108@qq.com>
2016-01-23 23:12:49 +0800

    10

--- Index/Runtime/Cache/Home/20af70db63cb4bc7fb75e2baa0bd9a04.php
+++ Index/Runtime/Cache/Home/20af70db63cb4bc7fb75e2baa0bd9a04.php
@@ -6,8 +6,15 @@
 	<script src="/hennessy/Public/js/jquery-2.1.4.min.js" ></script>
 	<link  rel="stylesheet" href="/hennessy/Public/css/bootstrap.min.css"/>
 	<script src="/hennessy/Public/js/bootstrap.js" ></script>
+	<style type="text/css">
+		p.p1{
+			font-size: 1.2em;
+			font-weight: bold;
+			font-family: 幼圆;
+		}
+	</style>
 </head>
 <body>
-相册
+	<p class="p1">>>相册列表</p>
 </body>
 </html>
